bccfc25ea9f50e0ae101f494fd2da021a8b2cb97
Amol Bais <amol.bais@idainfront.se>
2024-03-28 05:35:47 +0530

    New Patterns

--- src/patterns/Patterns.java
+++ src/patterns/Patterns.java
@@ -4,7 +4,7 @@ public class Patterns
 {
     public static void main(String[] args)
     {
-        thirteen();
+        sixteen();
     }
 
     /**
@@ -344,6 +344,135 @@ public class Patterns
 
             }
             System.out.println();
+        }
+    }
+
+    /**
+     *      *********
+     *       *     *
+     *        *   *
+     *         * *
+     *          *
+     *
+     * **/
+    public static void fourteen()
+    {
+        int N = 5;
+        for (int row = N; row >= 1 ; row--)
+        {
+            for (int space = 0; space <= (N - row); space++)
+            {
+                System.out.print(" ");
+            }
+            for (int coulmn = (2 * row - 1); coulmn >= 1; coulmn--)
+            {
+
+                if (coulmn == 1 || row == N || coulmn == (2 * row - 1))
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     *          *
+     *         * *
+     *        *   *
+     *       *     *
+     *       *      *
+     *        *   *
+     *         * *
+     *          *
+     *
+     * **/
+    public static void fifteen()
+    {
+        int N = 5;
+        for (int row = 1; row <= N ; row++)
+        {
+            for (int space = 0; space <= (N - row); space++)
+            {
+                System.out.print(" ");
+            }
+            for (int coulmn = 1; coulmn <= (2 * row - 1); coulmn++)
+            {
+
+                if (coulmn == 1 || coulmn == (2 * row - 1))
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+        for (int row = N; row >= 1 ; row--)
+        {
+            for (int space = 0; space <= (N - row); space++)
+            {
+                System.out.print(" ");
+            }
+            for (int coulmn = (2 * row - 1); coulmn >= 1; coulmn--)
+            {
+
+                if (coulmn == 1 || coulmn == (2 * row - 1))
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+    *                1
+     *             1   1
+     *           1   2   1
+     *         1   3   3   1
+     *       1   4   6   4   1
+    *
+    * */
+
+    public static void sixteen()
+    {
+        int N = 5;
+        int[][] triangle = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            triangle[i][0] = 1; // First column is always 1
+            triangle[i][i] = 1; // Last column is always 1
+
+            for (int j = 1; j < i; j++) {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        }
+
+        for (int row = 0; row < N; row++)
+        {
+            for (int space = 0; space <= (N - row); space++)
+            {
+                System.out.print(" ");
+            }
+            for (int col = 0; col <= row; col++) {
+                System.out.printf("%4d", triangle[row][col]); // Adjust spacing as needed
+            }
+            System.out.println();
+
         }
     }
 
