b7de8e3d2c75d13ca7233777159141a049709e71
Amol Bais <amol.bais@idainfront.se>
2024-08-15 13:13:02 +0530

    Sum of natural numbers
    Sum of natural numbers with multiple approches

---
+++ src/recursion/SumOfNaturalNumbers.java
@@ -0,0 +1,54 @@
+package recursion;
+
+public class SumOfNaturalNumbers
+{
+    public static void main(String[] args)
+    {
+        System.out.println(naiveApproach(10));
+        System.out.println(optimalApproach(10));
+        recursiveParameterizedApproach(10, 0);
+        System.out.println(recursiveFunctionalApproach(10));
+
+    }
+
+    private static int recursiveFunctionalApproach(int num)
+    {
+        if (num == 0)
+        {
+            return 0;
+        }
+        return num + recursiveFunctionalApproach(num - 1);
+    }
+    // TC : O (n)
+    // SC : O (n)
+
+    private static void recursiveParameterizedApproach(int num, int sum)
+    {
+        if (num < 0)
+        {
+            System.out.println(sum);
+            return;
+        }
+        recursiveParameterizedApproach(num - 1, sum + num);
+    }
+    // TC : O (n)
+    // SC : O (n)
+
+    private static int optimalApproach(int num)
+    {
+        return num * (num + 1) / 2;
+    } // TC : O (1)
+
+    private static int naiveApproach(int num)
+    {
+        int sum = 0;
+        for (int i = 0; i <= num; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    } // TC : O(n)
+
+
+
+}
