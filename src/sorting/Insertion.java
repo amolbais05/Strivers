90e313b77256cd621f2bbedaebdbd45702cb6d01
Amol Bais <amolbais05@gmail.com>
2024-08-28 23:00:03 +0530

    Insertion sort using recursion

--- src/sorting/Insertion.java
+++ src/sorting/Insertion.java
@@ -7,7 +7,8 @@ public class Insertion
     public static void main(String[] args)
     {
         int arr[] = {3 ,2 ,1, 4, 6,5};
-        IntStream.of(sort(arr)).forEach(System.out::print);
+        recursive(arr, arr.length, 0);
+        IntStream.of(arr).forEach(System.out::print);
     }
 
     private static int[] sort(int[] arr)
@@ -29,4 +30,21 @@ public class Insertion
     }
     // Avg and Worst : O(N^2)
     // Best if array is sorted O(N)
+
+    private static void recursive(int[] arr, int n, int i)
+    {
+            if (i == n)
+            {
+                return;
+            }
+            int j = i;
+            while (j > 0 && arr[j-1] > arr[j])
+            {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        recursive(arr, n, i + 1);
+    }
 }
