bb217e809b39f47e8a31ea1ca2bbb0e4c913bbef
Amol Bais <amolbais05@gmail.com>
2024-11-21 21:24:58 +0530

    Recursion interview problem

---
+++ src/recursion/Solve.java
@@ -0,0 +1,27 @@
+package recursion;
+
+public class Solve
+{
+    public static void main(String[] args)
+    {
+        System.out.println(foo(3, 5));
+    }
+
+    private static int foo(int x, int y)
+    {
+        if (y == 0)
+        {
+            return 1;
+        }
+        return bar(x, foo(x, y - 1));
+    }
+
+    private static int bar(int x, int y)
+    {
+        if (y == 0)
+        {
+            return 0;
+        }
+        return (x + bar(x , y - 1));
+    }
+}
