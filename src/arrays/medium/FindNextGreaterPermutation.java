4bddad8b00ff9baef9dd37fb4affb9ee45d184dd
Amol Bais <amolbais05@gmail.com>
2025-01-08 23:33:47 +0530

    No better solution in java

--- src/arrays/medium/FindNextGreaterPermutation.java
+++ src/arrays/medium/FindNextGreaterPermutation.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 public class FindNextGreaterPermutation
 {
+    // Better Solution : C++ STL Library
+
+
     public static List< Integer > nextGreaterPermutation(List< Integer > A) {
             int n = A.size(); // size of the array.
 
