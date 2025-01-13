44b417ed6e894b1797f4bd30663ca6ff70e57f26
Amol Bais <amolbais05@gmail.com>
2025-01-13 22:48:54 +0530

    better and optimal solution for set matrix zero

--- src/arrays/medium/SetMatrixZero.java
+++ src/arrays/medium/SetMatrixZero.java
@@ -6,8 +6,9 @@ public class SetMatrixZero
     public static void main(String[] args)
     {
         int[][] arr = {{1,1,1},{1,0,1},{1,1,1}};
-        bruteForce(arr);
-
+        //bruteForce(arr);
+        //better(arr);
+        optimal(arr);
     }
 
 
@@ -55,7 +56,80 @@ public class SetMatrixZero
         int length = matrix.length;
         int[] row = new int[length];
         int[] column = new int[length];
+
+
+        // First pass to mark the rows and columns that need to be zeroed
+        for (int rw = 0; rw < length; rw++)
+        {
+            for (int clm = 0; clm < length; clm++)
+            {
+                if (matrix[rw][clm] == 0)
+                {
+                    row[rw] = 1;
+                    column[clm] = 1;
+                }
+            }
+        }
+
+        // Second pass to zero out the marked rows and columns
+        for (int rw = 0; rw < length; rw++)
+        {
+            for (int clm = 0; clm < length; clm++)
+            {
+                if (row[rw] == 1 || column[clm] == 1)
+                {
+                    matrix[rw][clm] = 0;
+                }
+            }
+        }
+        printMatrix(length, matrix);
     }
+    // TC : O(2NM)
+    // SC : O(N) + O(M)
+
+    private static void optimal(int[][] matrix)
+    {
+        int length = matrix.length;
+        int col0 = 1;
+        // First pass to mark the rows and columns that need to be zeroed
+        for (int row = 0; row < length; row++)
+        {
+            for (int column = 0; column < length; column++)
+            {
+                if (matrix[row][column] == 0)
+                {
+                    matrix[row][0] = 0;
+
+                    if (column != 0)
+                    {
+                        matrix[0][column] = 0;
+                    }
+                    else
+                    {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+        // Second pass to zero out the marked rows and columns
+        for (int row = length - 1; row >= 0; row--)
+        {
+            for (int column = length - 1; column >= 1; column--)
+            {
+                if (matrix[row][0] == 0 || matrix[0][column] == 0)
+                {
+                    matrix[row][column] = 0;
+                }
+            }
+            if (col0 == 0)
+            {
+                matrix[row][0] = 0;
+            }
+        }
+        printMatrix(length, matrix);
+    }
+    // TC : O(2NM)
+    // SC : O(1)
 
 
     private static void printMatrix(int length, int[][] matrix)
