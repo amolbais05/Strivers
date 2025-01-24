59b554317c3b416c3a807a06d592a9c2b9ff3ad4
Amol Bais <amolbais05@gmail.com>
2025-01-24 23:05:43 +0530

    Pascal Triangle by iterative approach

---
+++ src/arrays/hard/PascalTriangle.java
@@ -0,0 +1,48 @@
+package arrays.hard;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PascalTriangle {
+
+    public static void main(String[] args) {
+        printPascalTriangle(5);
+    }
+
+    private static void printPascalTriangle(int n) {
+        int[][] pascalTriangle = new int[n][n];
+        for (int i = 0; i < n; i++)
+        {
+            pascalTriangle[i][0] = 1; // First column is always 1
+            pascalTriangle[i][i] = 1; // Last Column is always 1
+
+            for (int j = 1; j < i; j++)
+            {
+                pascalTriangle[i][j] = pascalTriangle[i - 1][j - 1] + pascalTriangle[i - 1][j];
+            }
+        }
+
+        // Convert matrix to List<List<Integer>> using streams
+        List<List<Integer>> listOfLists = Arrays.stream(pascalTriangle)
+                .map(row -> Arrays.stream(row) // Stream each row
+                        .boxed()    // Convert int to Integer
+                        .takeWhile(num -> num != 0) // Only take non-zero numbers
+                        .collect(Collectors.toList())) // Collect to List<Integer>
+                .collect(Collectors.toList()); // Collect to List<List<Integer>>
+
+
+        System.out.println(listOfLists);
+
+        }
+    }
+
+
+/**
+ *                1
+ *             1   1
+ *           1   2   1
+ *         1   3   3   1
+ *       1   4   6   4   1
+ *
+ * */
