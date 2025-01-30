e2ff761512d4e311d992e8c42037b14b745da5f8
Amol Bais <amolbais05@gmail.com>
2025-01-30 23:08:47 +0530

    better and optimal solution for 3sum

--- src/arrays/hard/ThreeSum.java
+++ src/arrays/hard/ThreeSum.java
@@ -33,6 +33,68 @@ public class ThreeSum {
     // TC : O(n^3)
     // SC : O(3 * k)
 
+    private static List<List<Integer>> better(int[] arr)
+    {
+        Set<List<Integer>> st = new HashSet<>();
+        for (int i = 0; i < arr.length; i++)
+        {
+            Set<Integer> hashset = new HashSet<>();
+            for (int j = i + 1; j < arr.length; j++)
+            {
+                int third = -(arr[i] + arr[j]);
+                if (hashset.contains(third))
+                {
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], third);
+                    temp.sort(null);
+                    st.add(temp);
+                }
+                hashset.add(arr[j]);
+            }
+        }
+        return new ArrayList<>(st);
+    }
+    // TC : O(n^2)
+    // SC : O(n)
+
+    private static List<List<Integer>> optimal(int n, int[] arr) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++)
+        {
+            //remove duplicates:
+            if (i != 0 && arr[i] == arr[i - 1]) continue;
+
+            //moving 2 pointers:
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum < 0)
+                {
+                    j++;
+                }
+                else if (sum > 0)
+                {
+                    k--;
+                }
+                else
+                {
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    //skip the duplicates:
+                    while (j < k && arr[j] == arr[j - 1]) j++;
+                    while (j < k && arr[k] == arr[k + 1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+    // TC : O(n^2)
+    // SC : O(1)
+
 
 
 }
