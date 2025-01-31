8c268635e76ec6501e9be2fe0a9f098c00dbd23e
Amol Bais <amol.bais@idainfront.se>
2025-01-31 11:25:50 +0530

    better solution for 4 sum

--- src/arrays/hard/FourSum.java
+++ src/arrays/hard/FourSum.java
@@ -1,9 +1,6 @@
 package arrays.hard;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class FourSum
 {
@@ -47,10 +44,46 @@ public class FourSum
     // TC : O(n^4)
     // SC : O(n)
 
-    private static List<List<Integer>> better(int[] arr, int target)
+    private static Set<List<Integer>> better(int[] arr, int target)
     {
         Set<List<Integer>> st = new HashSet<>();
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n ;j++)
+            {
+                HashSet<Long> hashset = new HashSet<>();
+                for (int k = j + 1; k < n; k++)
+                {
+                    long sum = arr[i] + arr[j];
+                    sum += arr[k];
+                    long fourth = target - sum;
+                    if (hashset.contains(fourth))
+                    {
+                        List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k], (int) fourth);
+                        temp.sort(null);
+                        st.add(temp);
+                    }
+                    hashset.add((long) arr[k]);
+                }
+            }
+        }
+        return st;
+    }
+    // TC : O(N3*log(M)), where N = size of the array, M = no. of elements in the set.
+    // SC : O(2 * no. of the quadruplets)+O(N)
+
+
+    private static List<List<Integer>> optimal(int[] arr, int target)
+    {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = arr.length;
+
         
+
+
+        return ans;
     }
 
 
