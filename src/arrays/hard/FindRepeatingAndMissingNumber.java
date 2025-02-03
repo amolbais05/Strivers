dd7a14202619a4a130a6af041aea99db8601a5b2
Amol Bais <amolbais05@gmail.com>
2025-02-03 23:32:01 +0530

    Find missing and repeating number
    optimal and brute force appraoch

--- src/arrays/hard/FindRepeatingAndMissingNumber.java
+++ src/arrays/hard/FindRepeatingAndMissingNumber.java
@@ -6,7 +6,7 @@ public class FindRepeatingAndMissingNumber
 {
     public static void main(String[] args)
     {
-        int[] arr = {4, 3, 6, 2, 1, 1};
+        int[] arr = {1, 2, 4, 6};
         int[] ans = optimal(arr);
         Arrays.stream(ans).forEach(System.out::print);
 
@@ -39,4 +39,50 @@ public class FindRepeatingAndMissingNumber
     }
     // TC : O (n)
     // SC : O (1)
+
+    private static int[] usingHashArray(int[] nums)
+    {
+        int[] ans = new int[2];
+        int[] hash = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++)
+        {
+            hash[nums[i]]++;
+        }
+        for (int i = 1; i < hash.length; i++)
+        {
+            if (hash[i] == 2)
+            {
+                ans[0] = i;
+            }
+            else if (hash[i] == 0)
+            {
+                ans[1] = i;
+            }
+        }
+        return ans;
+    }
+    // TC : O (n)
+    // SC : O (1)
+
+    private static int[] bruteForce(int[] nums)
+    {
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++)
+            {
+                if (nums[j] == i + 1)
+                    count++;
+            }
+            if (count == 2)
+                ans[0] = i + 1;  // Duplicate
+            else if (count == 0)
+                ans[1] = i + 1;  // Missing
+        }
+        return ans;
+    }
+    // TC : O (n^2)
+    // SC : O (1)
+
 }
