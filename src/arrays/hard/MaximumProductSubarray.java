31b66f3fc34f7816abe50b45c739563ef6392585
Amol Bais <amolbais05@gmail.com>
2025-02-08 22:04:39 +0530

    Maximum Product Subarray
    description

--- src/arrays/hard/MaximumProductSubarray.java
+++ src/arrays/hard/MaximumProductSubarray.java
@@ -1,5 +1,21 @@
 package arrays.hard;
 
+
+/**
+Problem Statement:
+Given an array that contains both negative and positive integers, find the maximum product subarray.
+
+Example 1:
+Input:  Nums = [1,2,3,4,5,0]
+Output: 120
+Explanation: In the given array, we can see 1󫎿󫶙 gives maximum product value.
+
+
+Example 2:
+Input: Nums = [1,2,-3,0,-4,-5]
+Output: 20
+Explanation: In the given array, we can see (-4)�(-5) gives maximum product value.
+* */
 public class MaximumProductSubarray
 {
     public static void main(String[] args)
