902bf9d2be0dd80505e6fbebcb01dd0ffe99acdf
Amol Bais <amolbais05@gmail.com>
2025-02-27 20:41:19 +0530

    Square root
    another brute force

--- src/binarysearch/onanswers/Sqrt.java
+++ src/binarysearch/onanswers/Sqrt.java
@@ -4,8 +4,9 @@ public class Sqrt
 {
     public static void main(String[] args)
     {
-        int n = 4;
+        int n = 36;
         System.out.println(bruteForce(n));
+        System.out.println(bruteForce2(n));
         System.out.println(better(n));
         System.out.println(optimal(n));
     }
@@ -15,6 +16,27 @@ public class Sqrt
         int ans = 0;
         // linear search on the answer space
         for (long i = 1; i <= n; i++)
+        {
+            long val = i * i;
+            if (val <= (long) n)
+            {
+                ans = (int) i;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return ans;
+    }
+    // TC : O(sqrt(n))
+    // SC : O(1)
+
+    private static int bruteForce2(int n)
+    {
+        int ans = 0;
+        // linear search on the answer space
+        for (long i = 1; i <= n / 2; i++)
         {
             long val = i * i;
             if (val <= (long) n)
