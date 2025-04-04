081ddb7ca1a705b8adbbd0f7cdba3bc21bb18fab
Amol Bais <amolbais05@gmail.com>
2025-04-04 23:14:11 +0530

    Aggressive cows
    optimal solution

--- src/binarysearch/onanswers/AggresiveCows.java
+++ src/binarysearch/onanswers/AggresiveCows.java
@@ -10,7 +10,7 @@ public class AggresiveCows
         int[] stalls = {0, 3, 4, 7, 10, 9};
         int k = 4;
         System.out.println(bruteForce(stalls, k));
-      //  System.out.println(optimal(stalls, k));
+        System.out.println(optimal(stalls, k));
     }
 
     private static int bruteForce(int[] stalls, int k)
@@ -28,7 +28,7 @@ public class AggresiveCows
         }
         return limit;
     }
-    // TC : O(n * log(n) + n * limit)
+    // TC : O(NlogN) + O(N *(max(stalls[])-min(stalls[])))
     // SC : O(1)
 
     private static boolean canWePlace(int[] stalls, int dist, int cows)
@@ -50,4 +50,27 @@ public class AggresiveCows
         }
         return false;
     }
+
+    private static int optimal(int[] stalls, int k)
+    {
+        int n = stalls.length;
+        Arrays.sort(stalls);
+        int low = 1;
+        int high = stalls[n - 1] - stalls[0];
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            if (canWePlace(stalls, mid, k))
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return high;
+    }
+    // TC : O(NlogN) + O(N * log(max(stalls[])-min(stalls[])))
+    // SC : O(1)
 }
