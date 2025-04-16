ab2fb71ca40f78e3c28f8fd6338f184b4db1be4c
Amol Bais <amolbais05@gmail.com>
2025-04-16 19:39:17 +0530

    Longest Palindromic Substring
    using two pointers

--- src/strings/LongestPalindromicSubstring.java
+++ src/strings/LongestPalindromicSubstring.java
@@ -5,6 +5,7 @@ public class LongestPalindromicSubstring
     public static void main(String[] args)
     {
         String s = "babad";
+        System.out.println(longestPalindromeUsingTwoPointers(s));
         System.out.println(longestPalindromeUsingDp(s));
     }
 
@@ -59,5 +60,40 @@ public class LongestPalindromicSubstring
         return dp[i][j];
     }
 
+    private static String longestPalindromeUsingTwoPointers(String s)
+    {
+        String ans = "";
+        for (int i = 0; i < s.length(); i++)
+        {
+            String oddString = expand(i, i, s);
+            String evenString = expand(i, i + 1, s);
 
+            if (evenString.length() > ans.length())
+            {
+                ans = evenString;
+            }
+
+            if (oddString.length() > ans.length())
+            {
+                ans = oddString;
+            }
+        }
+        return ans;
+    }
+    // TC : O(n^2)
+    // SC : O(1)
+
+    private static String expand(int i, int j, String s)
+    {
+        int left = i;
+        int right = j;
+
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right))
+        {
+            left--;
+            right++;
+        }
+
+        return s.substring(left + 1, right);
+    }
 }
