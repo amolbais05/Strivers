e05681c70e850915a619d6431eebda8f3707039b
Amol Bais <amolbais05@gmail.com>
2025-04-17 10:25:14 +0530

    Access element

---
+++ src/linkedlist/AccessElement.java
@@ -0,0 +1,20 @@
+package linkedlist;
+
+public class AccessElement
+{
+    public static void main(String[] args)
+    {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        System.out.println(head.data);
+        System.out.println(head.next.data);
+        System.out.println(head.next.next.data);
+        System.out.println(head.next.next.next.data);
+        System.out.println(head.next.next.next.next.data);
+        System.out.println(head.next.next.next.next.next.data); // null
+    }
+}
