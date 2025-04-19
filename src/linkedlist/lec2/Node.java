5b63735be5ace7806c27952a9aab2ccddf9e4e83
Amol Bais <amolbais05@gmail.com>
2025-04-19 16:43:34 +0530

    Doubly linked list node

--- src/linkedlist/lec2/Node.java
+++ src/linkedlist/lec2/Node.java
@@ -2,4 +2,21 @@ package linkedlist.lec2;
 
 public class Node
 {
+    int data;
+    Node next;
+    Node prev;
+
+    public Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public Node(int data, Node next, Node prev)
+    {
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
 }
