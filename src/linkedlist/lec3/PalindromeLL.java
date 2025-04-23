8a74ba4eec7054d54deca074c162262ff2cd2193
Amol Bais <amolbais05@gmail.com>
2025-04-23 18:33:23 +0530

    palindrome optimal

--- src/linkedlist/lec3/PalindromeLL.java
+++ src/linkedlist/lec3/PalindromeLL.java
@@ -39,5 +39,62 @@ public class PalindromeLL
     // TC : O (2N)
     // SC : O (N)
 
+    private static boolean isPalindrome(Node head)
+    {
+        if (head == null || head.next == null)
+        {
+            return true;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node newHead = reverse(slow);
+
+        Node first = head;
+        Node second = newHead;
+
+        while (second != null)
+        {
+            if (first.data != second.data)
+            {
+                reverse(newHead);
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+        reverse(newHead);
+        return true;
+    }
+    // TC : O (N)
+    // SC : O (1)
+
+
+    private static Node reverse(Node head)
+    {
+        if (head == null || head.next == null)
+        {
+            return head;
+        }
+
+        Node temp = head;
+        Node prev = null;
+
+        while (temp != null)
+        {
+            Node front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
 
 }
