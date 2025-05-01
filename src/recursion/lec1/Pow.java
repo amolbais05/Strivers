f56cb35ccd7793ab049e075c073b7257bf91843e
Amol Bais <amolbais05@gmail.com>
2025-05-01 15:42:48 +0530

    Pow

---
+++ src/recursion/lec1/Pow.java
@@ -0,0 +1,50 @@
+package recursion.lec1;
+
+public class Pow
+{
+    public static void main(String[] args)
+    {
+        System.out.println(myPow(2, 10));
+    }
+
+    private static double myPow(double x, int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        if (x == 1)
+        {
+            return 1;
+        }
+
+        long N = n; // Use long to handle Integer.MIN_VALUE
+        if (N < 0)
+        {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return power(x, N);
+    }
+
+    private static double power(double x, long n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+
+        double half = power(x, n / 2);
+        if (n % 2 == 0)
+        {
+            return half * half;
+        }
+        else
+        {
+            return x * half * half;
+        }
+    }
+}
+// TC = O(log n)
+// SC = O(log n)
