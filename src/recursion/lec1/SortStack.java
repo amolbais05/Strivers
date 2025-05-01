0c563420c313885373101b1a40382957515e94d9
Amol Bais <amolbais05@gmail.com>
2025-05-01 22:49:28 +0530

    Sort stack

---
+++ src/recursion/lec1/SortStack.java
@@ -0,0 +1,45 @@
+package recursion.lec1;
+
+import java.util.Stack;
+
+public class SortStack
+{
+    private static void sortStack(Stack<Integer> stack)
+    {
+        if (!stack.isEmpty())
+        {
+            int temp = stack.pop();
+            sortStack(stack);
+            insert(stack, temp);
+        }
+    }
+
+    private static void insert(Stack<Integer> stack, int value)
+    {
+        if (stack.isEmpty() || stack.peek() < value)
+        {
+            stack.push(value);
+        }
+        else
+        {
+            int val = stack.pop();
+            insert(stack, value);
+            stack.push(val);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(5);
+        stack.push(1);
+        stack.push(0);
+        stack.push(2);
+
+        sortStack(stack);
+        while (!stack.isEmpty())
+        {
+            System.out.println(stack.pop());
+        }
+    }
+}
