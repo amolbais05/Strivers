3843e570529516d6406ed30954b3f009360739b7
Amol Bais <amolbais05@gmail.com>
2025-08-08 08:43:14 +0530

    Sub Sequnce Sum Count

---
+++ src/recursion/SubSequnceSumCount.java
@@ -0,0 +1,29 @@
+package recursion;
+
+
+import java.util.ArrayList;
+
+public class SubSequnceSumCount
+{
+
+    private static int sumOfSubSubSequence(int[] arr, int index, int targetSum, int currentSum, ArrayList<Integer> current)
+    {
+        if (arr.length == index)
+        {
+            return (targetSum == currentSum) ? 1 : 0;
+        }
+
+        // pick the element
+        current.add(arr[index]);
+        int pick = sumOfSubSubSequence(arr, index + 1, targetSum, currentSum + arr[index], current);
+        current.remove(current.size() - 1);
+
+        // not pick the element
+        int notPick = sumOfSubSubSequence(arr, index + 1, targetSum, currentSum, current);
+
+        return pick + notPick;
+    }
+    // TC : O (2^n)
+    // SC : O (n)
+
+}
