561c6e6a3b35a7335a4460bbdfbd19ed162d20b4
Amol Bais <amolbais05@gmail.com>
2025-08-13 07:42:48 +0530

    Subset Sum-I

---
+++ src/recursion/lec2/SubSetSumI.java
@@ -0,0 +1,32 @@
+package recursion.lec2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SubSetSumI
+{
+    public static void main(String[] args)
+    {
+        int[] arr = {3, 1, 2};
+        int n = arr.length;
+        List<Integer> result = new ArrayList<>();
+        subsetSum(0, 0, arr, n, result);
+        Collections.sort(result);
+
+    }
+
+    private static void subsetSum(int index, int sum, int[] arr, int n, List<Integer> result)
+    {
+        if (index == n)
+        {
+            result.add(sum);
+            return;
+        }
+        // pick the element
+        subsetSum(index + 1, sum + arr[index], arr, n, result);
+
+        // not pick the element
+        subsetSum(index + 1, sum, arr, n, result);
+    }
+}
