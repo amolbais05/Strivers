90e780d58b68d7af157d099b82ab2843b5bf52c0
Amol Bais <amolbais05@gmail.com>
2025-08-20 11:23:57 +0530

    Print all permutation without using extra space

--- src/recursion/lec1/PrintAllPermutation.java
+++ src/recursion/lec1/PrintAllPermutation.java
@@ -9,7 +9,7 @@ public class PrintAllPermutation
     // Approach 1 : Using extra space
     // SC : O (n) + O (n)
     // TC : O (n! * n)
-    private static void printPermutation(int[] nums, List<Integer> ds, List<List<Integer>> ans, boolean[] freq)
+    private static void permute(int[] nums, List<Integer> ds, List<List<Integer>> ans, boolean[] freq)
     {
 
         if (ds.size() == nums.length)
@@ -24,10 +24,43 @@ public class PrintAllPermutation
             {
                 freq[i] = true;
                 ds.add(nums[i]);
-                printPermutation(nums, ds, ans, freq);
+                permute(nums, ds, ans, freq);
                 ds.remove(ds.size() - 1);
                 freq[i] = false;
             }
         }
     }
+
+    // Approach 2 : Without using extra space
+    // SC : O (1)
+    // TC : O (n! * n)
+    private static void permute(int index, int[] nums, List<List<Integer>> ans)
+    {
+        if (index == nums.length)
+        {
+            List<Integer> ds = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++)
+            {
+                ds.add(nums[i]);
+            }
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for (int i = index; i < nums.length; i++)
+        {
+            swap(nums, i, index);
+            permute(index + 1, nums, ans);
+            swap(nums, i, index);
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
 }
