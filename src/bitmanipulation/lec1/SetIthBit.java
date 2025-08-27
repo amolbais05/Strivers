6c2f0cdc348d801df7368beebfa67908fb52c5b3
Amol Bais <amolbais05@gmail.com>
2025-08-27 11:49:27 +0530

    Set Ith Bit

---
+++ src/bitmanipulation/lec1/SetIthBit.java
@@ -0,0 +1,19 @@
+package bitmanipulation.lec1;
+
+public class SetIthBit
+{
+    public static void main(String[] args)
+    {
+        System.out.println(setIthBit(9, 2));
+        System.out.println(setIthBit(10, 2));
+        System.out.println(setIthBit(10, 1));
+        System.out.println(setIthBit(10, 0));
+    }
+
+    private static int setIthBit(int n, int i)
+    {
+        return n | (1 << i);
+    }
+    // TC : O(1)
+    // SC : O(1)
+}
