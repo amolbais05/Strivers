b5e20171a31c5e7c34566a23d5d3dea1ee204b9b
Amol Bais <amolbais05@gmail.com>
2025-10-14 07:03:00 +0530

    BinaryHeap

---
+++ src/heaps/BinaryHeap.java
@@ -0,0 +1,169 @@
+package heaps;
+
+public class BinaryHeap
+{
+    private int[] arr;
+    private int size;
+    private int capacity;
+
+    public BinaryHeap(int capacity)
+    {
+        this.capacity = capacity;
+        this.size = 0;
+        this.arr = new int[capacity];
+    }
+
+
+    public int getParent(int i)
+    {
+        return (i - 1) / 2;
+    }
+
+    public int getLeftChild(int i)
+    {
+        return 2 * i + 1;
+    }
+
+    public int getRightChild(int i)
+    {
+        return 2 * i + 2;
+    }
+
+    public void insert(int value)
+    {
+        if (size == capacity)
+        {
+            System.out.println("Binary Heap is full");
+            return;
+        }
+
+        // Insert  the new element at the end
+        arr[size] = value;
+
+        // store the index of the new element
+        int i = size;
+
+        // increase the size of the heap
+        size++;
+
+        // Fix the min heap property if it is violated
+        while (i != 0 && arr[getParent(i)] > arr[i])
+        {
+            int temp = arr[getParent(i)];
+            arr[getParent(i)] = arr[i];
+            arr[i] = temp;
+            i = getParent(i);
+        }
+
+    }
+
+    public void minHeapify(int index)
+    {
+        int left = getLeftChild(index);
+        int right = getRightChild(index);
+        int smallest = index;
+
+        if (left < size && arr[left] < arr[index])
+        {
+            smallest = left;
+        }
+
+        if (right < size && arr[right] < arr[smallest])
+        {
+            smallest = right;
+        }
+
+        if (smallest != index)
+        {
+            int temp = arr[smallest];
+            arr[smallest] = arr[index];
+            arr[index] = temp;
+            minHeapify(smallest);
+        }
+    }
+
+    public int extractMin()
+    {
+        if (size == 0)
+        {
+            return Integer.MAX_VALUE;
+        }
+
+        if (size == 1)
+        {
+            size--;
+            return arr[0];
+        }
+
+        int root = arr[0];
+
+        // Copy the last element to the root
+        arr[0] = arr[size - 1];
+        size--;
+        // Heapify the root
+        minHeapify(0);
+        return root;
+    }
+
+    public void decreaseKey(int index, int value)
+    {
+        arr[index] = value;
+        // Fix the min heap property if it is violated
+        while (index != 0 && arr[getParent(index)] > arr[index])
+        {
+            int temp = arr[getParent(index)];
+            arr[getParent(index)] = arr[index];
+            arr[index] = temp;
+            index = getParent(index);
+        }
+    }
+
+    public void deleteKey(int index)
+    {
+        decreaseKey(index, Integer.MIN_VALUE);
+        extractMin();
+    }
+
+    public int getMin()
+    {
+        return arr[0];
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public boolean isEmpty()
+    {
+        return size == 0;
+    }
+
+    public static void main(String args[])
+    {
+        BinaryHeap h = new BinaryHeap(20);
+
+        h.insert(4);
+        h.insert(1);
+        h.insert(2);
+        h.insert(6);
+        h.insert(7);
+        h.insert(3);
+        h.insert(8);
+        h.insert(5);
+
+        System.out.println("Min value is " + h.getMin());
+
+        h.insert(-1);
+        System.out.println("Min value is " + h.getMin());
+
+        h.decreaseKey(3, -2);
+        System.out.println("Min value is (decreaseKey) " + h.getMin());
+
+        h.extractMin();
+        System.out.println("Min value is (extractMin) " + h.getMin());
+
+        h.deleteKey(0);
+        System.out.println("Min value is (deleteKey) " + h.getMin());
+    }
+}
