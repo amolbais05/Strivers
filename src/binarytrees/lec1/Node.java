563023950b11158cda120a1f383824c2d14fb49c
Amol Bais <amolbais05@gmail.com>
2025-10-31 09:40:23 +0530

    Representation of binary tree

---
+++ src/binarytrees/lec1/Node.java
@@ -0,0 +1,29 @@
+package binarytrees.lec1;
+
+public class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data)
+    {
+        this.data = data;
+    }
+
+    public static void main(String[] args)
+    {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.right.left = new Node(5);
+    }
+    /*
+     *      1
+     *    /   \
+     *   2    3
+     *       /
+     *      5
+     *
+     * */
+}
