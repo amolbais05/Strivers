7d6b106c03dd1489f506ae94a1f4d7edd7ca03ce
Amol Bais <amolbais05@gmail.com>
2025-11-01 12:59:11 +0530

    Refactor

--- src/binarytrees/lec1/LevelOrderTraversal.java
+++ src/binarytrees/lec1/LevelOrderTraversal.java
@@ -46,28 +46,4 @@ public class LevelOrderTraversal
         return result;
     }
 
-
-    public class TreeNode
-    {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode()
-        {
-        }
-
-        TreeNode(int val)
-        {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
 }
--- src/binarytrees/lec1/TreeNode.java
+++ src/binarytrees/lec1/TreeNode.java
@@ -2,9 +2,9 @@ package binarytrees.lec1;
 
 public class TreeNode
 {
-    int val;
-    TreeNode left;
-    TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
     public TreeNode(int data)
     {
