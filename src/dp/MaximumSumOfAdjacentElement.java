7b70d0e45bb14254b350824443d94dbd470df8b8
Amol Bais <amolbais05@gmail.com>
2026-03-21 23:40:28 +0530

    Space optimization approach

--- src/dp/MaximumSumOfAdjacentElement.java
+++ src/dp/MaximumSumOfAdjacentElement.java
@@ -47,5 +47,37 @@ public class MaximumSumOfAdjacentElement
 
     }
 
+    // Space optimization
+    public int solveSpaceOptimization(int[] arr)
+    {
+        int n = arr.length;
+        if (n == 0)
+        {
+            return 0;
+        }
+        if (n == 1)
+        {
+            return arr[0];
+        }
+
+        int prev2 = 0;
+        int prev = arr[0];
+
+        for (int i = 1; i < n; i++)
+        {
+            int include = arr[i] + prev2;
+            int exclude = prev;
+
+            int curr = Math.max(include, exclude);
+
+            prev2 = prev;
+            prev = curr;
+
+        }
+
+        return prev;
+
+    }
+
 
 }
